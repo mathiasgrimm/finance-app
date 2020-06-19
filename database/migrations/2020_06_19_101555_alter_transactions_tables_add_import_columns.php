fe89171186c02c020594db45544b16070946ce36
Mathias Grimm <mathiasgrimm@gmail.com>
2020-06-19 07:24:56 -0300

    - adding migrations for the csv imports - adding migration to the transactions table regarding the csv imports

---
+++ database/migrations/2020_06_19_101555_alter_transactions_tables_add_import_columns.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AlterTransactionsTablesAddImportColumns extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->unsignedBigInteger('transaction_import_id')->nullable()->after('id');
+            $table->boolean('is_importing')->nullable()->after('transaction_import_id');
+
+            $table->foreign('transaction_import_id')->references('id')->on('transaction_imports');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->dropForeign(['transaction_import_id']);
+            $table->dropColumn('is_importing');
+        });
+    }
+}
