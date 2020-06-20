16d7060e547db81d9e395a5db2a87e6331fb34b4
Mathias Grimm <mathiasgrimm@gmail.com>
2020-06-20 10:35:41 -0300

    removing unused imports

--- app/Jobs/ProcessCsvImport.php
+++ app/Jobs/ProcessCsvImport.php
@@ -9,7 +9,6 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Validation\ValidationException;
 
 class ProcessCsvImport implements ShouldQueue
 {
