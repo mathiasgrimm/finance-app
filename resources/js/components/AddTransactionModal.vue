23b353de77005589aeeb30d119cf47fe31d62756
Mathias Grimm <mathiasgrimm@gmail.com>
2020-06-21 17:51:07 -0300

    refactoring modals to use the BaseModal

--- resources/js/components/AddTransactionModal.vue
+++ resources/js/components/AddTransactionModal.vue
@@ -1,83 +1,82 @@
 <template>
-    <div
-        class="h-screen fixed inset-0 w-full flex items-center justify-center bg-smoke shadow"
-        v-if="value"
+    <base-modal
+        :show="localShow"
+        @open="open"
+        @close="close"
     >
-        <div
-            class="z-50 flex items-center justify-center"
-        >
-            <div class="bg-white rounded shadow max-h-screen text-center">
-                <div class="mb-4 pl-10 py-8 border-b text-left">
-                    <h1 class="text-2xl font-semibold">Add Balance Entry</h1>
+        <template slot="title">
+            <h1 class="text-2xl font-semibold">Add Balance Entry</h1>
+        </template>
+
+        <template slot="content">
+            <form>
+                <div class="flex">
+                    <div class="w-full md:w-4/12 text-left">
+                        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                            Label
+                        </label>
+                        <input
+                            class="w-full py-3 px-4 mb-3 text-sm appearance-none block text-gray-700 border border-gray-300 rounded leading-tight focus:outline-none"
+                            type="text"
+                            placeholder="Car Insurance"
+                            v-model="form.label"
+                        >
+                    </div>
+
+                    <div class="w-full md:w-5/12 ml-3 text-left">
+                        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                            Date
+                        </label>
+
+                        <!-- there is a padding issue with this datetime-local, hard-coding via style to get it well aligned -->
+                        <input
+                            style="padding-top:10px; padding-bottom: 10px;"
+                            class="py-3 px-4 text-sm appearance-none block w-full text-gray-700 border border-gray-300 rounded focus:outline-none focus:bg-white focus:border-gray-500"
+                            type="datetime-local"
+                            v-model="form.transaction_at"
+                        >
+                    </div>
+
+                    <div class="w-full md:w-3/12 ml-3 text-left">
+                        <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                            Amount
+                        </label>
+                        <input
+                            class="py-3 px-4 text-sm appearance-none block w-full text-gray-700 border border-gray-300 rounded focus:outline-none focus:bg-white focus:border-gray-500"
+                            type="number"
+                            placeholder="$ - 500.00"
+                            v-model="form.amount"
+                        >
+                    </div>
                 </div>
-                <div class="mb-8">
-                    <form class="w-full">
-                        <div class="flex mb-6 border-b pb-4 pt-8">
-                            <div class="w-full md:w-1/3 pl-8 mb-6 text-left">
-                                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                                    Label
-                                </label>
-                                <input
-                                    class="w-full py-3 px-4 mb-3 text-sm appearance-none block text-gray-700 border border-gray-300 rounded leading-tight focus:outline-none"
-                                    type="text"
-                                    placeholder="Car Insurance"
-                                    v-model="form.label"
-                                >
-                            </div>
+            </form>
+        </template>
 
-                            <div class="w-full md:w-1/3 ml-3 text-left">
-                                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                                    Date
-                                </label>
+        <template slot="bottom">
+            <div class=" flex justify-end items-center">
+                <button
+                    class="py-5 px-8 rounded items-center text-xs font-bold tracking-wider bg-gray-300 hover:bg-gray-400 text-gray-600"
+                    @click.prevent="close"
+                >CANCEL
+                </button>
 
-                                <!-- there is a padding issue with this datetime-local, hard-coding via style to get it well aligned -->
-                                <input
-                                    style="padding-top:10px; padding-bottom: 10px;"
-                                    class="py-3 px-4 text-sm appearance-none block w-full text-gray-700 border border-gray-300 rounded focus:outline-none focus:bg-white focus:border-gray-500"
-                                    type="datetime-local"
-                                    v-model="form.transaction_at"
-                                >
-                            </div>
-
-                            <div class="w-full md:w-1/3 ml-3 mr-8 text-left">
-                                <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
-                                    Amount
-                                </label>
-                                <input
-                                    class="py-3 px-4 text-sm appearance-none block w-full text-gray-700 border border-gray-300 rounded focus:outline-none focus:bg-white focus:border-gray-500"
-                                    type="number"
-                                    placeholder="$ - 500.00"
-                                    v-model="form.amount"
-                                >
-                            </div>
-                        </div>
-
-                        <div class=" pr-6 flex justify-end items-center">
-                            <button
-                                class="py-5 px-6 rounded items-center text-xs font-bold tracking-wider bg-gray-300 hover:bg-gray-400 text-gray-600"
-                                @click.prevent="$emit('cancel');"
-                            >CANCEL
-                            </button>
-
-                            <button
-                                class="py-5 px-6 ml-4 rounded items-center text-xs font-bold tracking-wider bg-primary hover:bg-blue-900 text-white"
-                                @click.prevent="addTransaction"
-                            >SAVE ENTRY
-                            </button>
-                        </div>
-                    </form>
-
-
-                </div>
+                <button
+                    class="py-5 px-8 ml-4 rounded items-center text-xs font-bold tracking-wider bg-primary hover:bg-blue-900 text-white"
+                    @click.prevent="addTransaction"
+                >SAVE ENTRY
+                </button>
             </div>
-        </div>
-    </div>
+        </template>
+
+    </base-modal>
 </template>
 
 <script>
+    import BaseModal from "./BaseModal";
     export default {
         data() {
             return {
+                localShow: false,
                 form: {
                     label: null,
                     transaction_at: null,
@@ -86,12 +85,40 @@
             }
         },
 
-        props: [
-            'value',
-            'userId'
-        ],
+        components: {
+            BaseModal,
+        },
+
+        props: {
+            show: {
+                required: true,
+            },
+
+            userId: {
+                required: true,
+            }
+        },
+
+        beforeMount() {
+            this.localShow = this.show;
+        },
 
         methods: {
+            open() {
+                this.reset();
+                this.localShow = true;
+                this.$nextTick(() => {
+                    this.$emit('open');
+                });
+            },
+
+            close() {
+                this.localShow = false;
+                this.$nextTick(() => {
+                    this.$emit('close');
+                });
+            },
+
             reset() {
                 this.form = {
                     label: null,
@@ -100,14 +127,12 @@
                 }
             },
 
-            toggleDisplay() {
-                this.reset();
-                this.$emit('input', !this.value);
-            },
-
             addTransaction() {
                 axios.post(`/users/${this.userId}/transactions`, this.form).then(res => {
-                    this.$emit('success');
+                    this.close();
+                    this.$nextTick(() => {
+                        this.$emit('success');
+                    })
                 }).catch(err => {
                     // TODO
                     alert('Something went wrong');
@@ -116,9 +141,3 @@
         }
     }
 </script>
-
-<style scoped>
-    .bg-smoke {
-        background-color: rgba(0, 0, 0, .8);
-    }
-</style>
