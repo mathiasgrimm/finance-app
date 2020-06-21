64cc83f616d8f70b8aeeee4f132debe01dfc423c
Mathias Grimm <mathiasgrimm@gmail.com>
2020-06-21 17:36:41 -0300

    adding helper method to check whether the user is currently importing anything or not and returning the current transactionImport in case its importing

--- app/TransactionImport.php
+++ app/TransactionImport.php
@@ -7,4 +7,21 @@ use Illuminate\Database\Eloquent\Model;
 class TransactionImport extends Model
 {
     protected $guarded = [];
+
+    /**
+     * @param int|User $user
+     * @return null|TransactionImport
+     */
+    public static function currentlyImporting($user)
+    {
+        if (is_numeric($user)) {
+            $user = User::findOrFail($user);
+        }
+
+        $transactionImport = TransactionImport::where('user_id', $user->id)
+            ->whereNull('finished_at')
+            ->first();
+
+        return $transactionImport;
+    }
 }
