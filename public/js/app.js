926f1cb91fe3a277a5ef3cb7492978146cda3b7a
Mathias Grimm <mathiasgrimm@gmail.com>
2020-06-21 23:14:12 -0300

    assets compilation

--- public/js/app.js
+++ public/js/app.js
@@ -62214,8 +62214,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.Pusher = __webpack_require__(/*! pusher-js */ "./node_modules/pusher-js/dist/web/pusher.js");
 window.Echo = new laravel_echo__WEBPACK_IMPORTED_MODULE_0__["default"]({
   broadcaster: 'pusher',
-  key: "0de0f837d3006cd9056b",
-  cluster: "mt1",
+  key: "6029ea78a13698e383c4",
+  cluster: "eu",
   forceTLS: true
 });
 
